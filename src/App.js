2730dd0cb4acede2e18ac44388dd9029bb619a3c
Rahul Bhadja <rahulbhadja@gmail.com>
2021-10-15 12:19:25 +0530

    updating app with header

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import shopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
-    <div className='App'>
+    <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={shopPage} />
